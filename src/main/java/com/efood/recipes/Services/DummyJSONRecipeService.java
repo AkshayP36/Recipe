6650702dd4815e4c775e1ea4e9f820530a354b56
Akshay Patil <studyakshaystudy@outlook.com>
2025-01-31 01:16:18 +0800

    Get All Recipes

--- src/main/java/com/efood/recipes/Services/DummyJSONRecipeService.java
+++ src/main/java/com/efood/recipes/Services/DummyJSONRecipeService.java
@@ -5,6 +5,9 @@ import com.efood.recipes.Models.Recipe;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class DummyJSONRecipeService implements RecipeService{
     RestTemplate restTemplate;
@@ -17,5 +20,34 @@ public class DummyJSONRecipeService implements RecipeService{
     public Recipe getASingleRecipe(Long id) {
         RecipeDto recipe = restTemplate.getForObject("https://dummyjson.com/recipes/"+id, RecipeDto.class);
         return recipe.getRecipeData();
+    }
+
+    @Override
+    public List<Recipe> getAllRecipes() {
+        try{
+            RecipeDto[] recipeDtos = restTemplate.getForObject("https://dummyjson.com/recipes", RecipeDto[].class);
+            if(recipeDtos!=null){
+                List<Recipe> recipe = List.of(recipeDtos).stream()
+                        .map(RecipeDto::getRecipeData)
+                        .collect(Collectors.toList());
+
+                return recipe;
+            }else{
+                throw new RuntimeException("No Recipes yet");
+            }
+
+        }catch(Exception e){
+            throw new RuntimeException("Error fetching recipe list");
+        }
+
+    }
+
+    @Override
+    public List<String> getAllRecipesTags() {
+        RecipeDto[] recipeDtos = restTemplate.getForObject("https://dummyjson.com/recipes/tags", RecipeDto[].class);
+//        List<String> tagList = List.of(recipeDtos).stream()
+//                .map(RecipeDto::getTags)
+//                .collect(Collectors.toList());
+        return null;
     }
 }
--- src/main/java/com/efood/recipes/Services/RecipeService.java
+++ src/main/java/com/efood/recipes/Services/RecipeService.java
@@ -2,6 +2,10 @@ package com.efood.recipes.Services;
 
 import com.efood.recipes.Models.Recipe;
 
+import java.util.List;
+
 public interface RecipeService {
     public Recipe getASingleRecipe(Long id);
+    public List<Recipe> getAllRecipes();
+    public List<String> getAllRecipesTags();
 }
